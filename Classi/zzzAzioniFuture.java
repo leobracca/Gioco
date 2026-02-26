cdf1de867d0edb9f09608b10fcefe67a83e0c8c4
leobracca <braccalenti.leonardo@franchettisalviani.net>
2026-02-26 20:10:04 +0100

    Sistemazioni per il futuro
    -Aggiunto nella classe commenti delle informazioni da implementare
    obbligatoriamente la prossima volta
    -Iniziato organizzazione file csv

--- Classi/zzzAzioniFuture.java
+++ Classi/zzzAzioniFuture.java
@@ -2,6 +2,7 @@ package Gioco.Classi;
 
 public class zzzAzioniFuture {
     //1. Sistemare il main, creare un metodo nel gestore per assegnazione dei personaggi ai giocatori
-    //2. Sistemare il file manager, migliorare il controllo dei personaggi scelti (riga 16)
-    //3. Migliorare il sistema di combattimento, farli perdere vita
+    //2. Sistemare il file manager, leggere tutti i personaggi e poi assegnarli al giocatore nel gestore
+    //3. Inserire piu informazioni nel file come ad esempio vita, recupero vita
+    //4. Migliorare il sistema di combattimento, farli perdere vita
 }
