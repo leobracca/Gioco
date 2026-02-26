f4336d08931660f8a9c9f35662a46b28701518e0
leobracca <braccalenti.leonardo@franchettisalviani.net>
2026-02-27 00:27:03 +0100

    Update zzzAzioniFuture.java

--- Classi/zzzAzioniFuture.java
+++ Classi/zzzAzioniFuture.java
@@ -1,6 +1,7 @@
 package Gioco.Classi;
 
  class zzzAzioniFuture {
-    //1. Inserire piu informazioni nel file come ad esempio danni, recupero vita
-    //2. Migliorare il sistema di combattimento, farli perdere vita
+    //1.Organizzare meglio il Gestore, creare un metodo e dividere la lettura del file con l'input
+    //2. Inserire piu informazioni nel file come ad esempio danni, recupero vita
+    //3. Migliorare il sistema di combattimento, farli perdere vita
 }
